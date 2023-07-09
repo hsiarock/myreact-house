3b7b42f2b511df7b4868e848eb54e8ac70e132e1
David Hsia <hsiarock@gmail.com>
2023-07-09 17:21:31 -0400

    add Axios to call Rest service

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { FilterHouseTable } from './components/FilterHouseTable.js';
+import { axios } from 'axios';
 
 
 export default function App() {
@@ -13,5 +15,30 @@ export default function App() {
     { zip: "07030", address: "5 Linda Lane", price: "200,000"}
   ];
 
-  return <FilterHouseTable houses={DEMO_HOUSES} />;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+         //.then((response) => response.json())
+         .then((resp) => {
+            console.log(resp.data.data);
+            setPosts(resp.data.data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+        }, []);
+
+
+  return (
+    <div>
+      <h2>Axios library returns</h2>
+      <ol>
+         { 
+          posts.map((item) =><li key={item?.userId}>{item?.title} - {item?.body}</li>)
+         }
+      </ol>
+      <FilterHouseTable houses={DEMO_HOUSES} />;
+    </div>
+  )
 }
